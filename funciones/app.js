718bc4834d5c0780cbbac24a4da4bdf305c1f6eb
kainfelyne <rainingblues13@gmail.com>
2022-04-07 17:02:20 -0500

    update

--- funciones/app.js
+++ funciones/app.js
@@ -1,9 +1,20 @@
 var a = 30
 
 function primeraFuncion(){
-    var a = 20
+    
+    
+    var a = 20 // Si no existe la variable "a" dentro de la función, lo toma del entorno global
 
     console.log(a)
 }
 
 primeraFuncion()
+
+
+function segundaFuncion(){
+    
+}
+
+var b = segundaFuncion()
+
+console.log(b)
